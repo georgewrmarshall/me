166a292695a2345c38ce831a73b53b7bae0b73d2
George Marshall <georgewrmarshall@gmail.com>
2020-09-03 11:19:16 -0700

    Updates

---
+++ component---src-pages-work-js-e35f9875b2cdeae79fd1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{Ofmp:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),r=t("Kvkj");n.default=function(){return l.a.createElement(l.a.Fragment,null,l.a.createElement(r.r,{title:"Home"}),l.a.createElement(r.k,null,l.a.createElement(r.n,null),l.a.createElement(r.s,null),l.a.createElement(r.f,null)))}}}]);
+//# sourceMappingURL=component---src-pages-work-js-e35f9875b2cdeae79fd1.js.map
