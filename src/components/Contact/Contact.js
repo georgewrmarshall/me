de86adbf4dbf87949c68e9478f917e465c412e2f
George Marshall <georgewrmarshall@gmail.com>
2020-09-16 17:26:08 -0700

    Fixing contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // Components
-import { Container, Hero, Main } from '../../components';
+import { Container, Hero, Main, Headline, Body } from '..';
 
 // Styles
 import { StyledContact } from './styles';
@@ -13,17 +13,17 @@ export const Contact = () => {
         <Hero header="Contact" description="Drop me a line" />
         <Main>
           <section>
-            <p css={`margin-bottom: 3rem;`}>
+            <Body css={`margin-bottom: 3rem;`}>
               I'm currently living in the small surf town of <a href="https://www.google.com/maps/place/Ucluelet,+BC/@48.9410902, -125.5991609,13z/data=!3m1!4b1!4m5!3m4!1s0x548973b97150e2bb:0xdb5f97b887c7db84!8m2!3d48.9415997!4d-125.5463446" target="_blank" rel="noopener noreferrer">Ucluelet, BC</a>. I always enjoy hearing about new projects so send me an email or connect with me on <a href="https://www.linkedin.com/in/georgewrmarshall/">Linkedin</a>.
-            </p>
-            <h2>You can contact me via</h2>
-            <p css={`margin-bottom: 3rem;`}>
+            </Body>
+            <Headline>You can contact me via</Headline>
+            <Body css={`margin-bottom: 3rem;`}>
               <strong>m.</strong> +1 236 886 7214 <br />
               <strong>e.</strong> georgewrmarshall@gmail.com <br />
-            </p>
-            <p>
+            </Body>
+            <Body>
               Thanks for visiting! <span role="img" aria-label="sheep">✌️</span>
-            </p>
+            </Body>
           </section>
         </Main>
       </Container>
